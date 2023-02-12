1aacb7af2e17f411f507788bdb541c4f6fad1ed5
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2023-02-12 23:34:24 +0100

    Added some articles to db using  interactive shell

--- articles/models.py
+++ articles/models.py
@@ -6,8 +6,11 @@ from django.db import models
 #Model describing an article element
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField
-    body = models.TextField
+    slug = models.SlugField()
+    body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     #add thumb
     #add author
+
+    def __str__(self):
+        return self.title
