34feff370d2f800695feca284bcebb26c141da52
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2023-02-12 23:02:54 +0100

    Added 'Article' model class and initiated migrations

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+#HELP : https://docs.djangoproject.com/en/4.1/ref/models/fields/#module-django.db.models.fields
 # Create your models here.
+
+#Model describing an article element
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField
+    body = models.TextField
+    date = models.DateTimeField(auto_now_add=True)
+    #add thumb
+    #add author
