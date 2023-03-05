3bef789a8b9790f6ccbebe1e886c7d2f37b03848
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2023-03-05 01:48:44 +0100

    Implemented url params to /articles/

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.article_list)
+    path('', views.article_list),
+    path('<slug:slug>/', views.article_details)
     
 ]
