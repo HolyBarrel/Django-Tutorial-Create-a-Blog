588c25e906bebd74d4b0b36065d1c7ac0e29e298
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2023-03-10 23:13:19 +0100

    Added functionality for users to sign up

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup', views.signup_view, name="signup"),
+    path('signup/', views.signup_view, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def signup_view(request):
-    form = UserCreationForm
-    return render(request, 'accounts/signup.html', {'form': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #log user in
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
     
+    #occurrs finally if there is a get-request or the post-request was sent on an invalid form
+    return render(request, 'accounts/signup.html', {'form': form})
+
