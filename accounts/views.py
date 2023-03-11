80a2a459b41d40d721a728bca99692b25a948562
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2023-03-11 13:02:27 +0100

    Implemented functionaity for users to log in

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -7,8 +8,8 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            #log user in
+            user = form.save()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
@@ -20,7 +21,8 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            #TODO log in user
+            user = form.get_user()
+            login(request, user)
             return redirect('articles:list')
 
     else: 
