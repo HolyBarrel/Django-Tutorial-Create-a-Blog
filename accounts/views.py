5b0255b3a53db1c75ad65ab17a7dea76595ad439
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2023-03-11 12:49:42 +0100

    Added login form for registered users

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
@@ -16,3 +16,14 @@ def signup_view(request):
     #occurrs finally if there is a get-request or the post-request was sent on an invalid form
     return render(request, 'accounts/signup.html', {'form': form})
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #TODO log in user
+            return redirect('articles:list')
+
+    else: 
+        form = AuthenticationForm()
+    
+    return render(request, 'accounts/login.html', {'form': form})
