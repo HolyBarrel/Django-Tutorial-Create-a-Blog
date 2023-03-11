b96c2d9a007f5a6f7a68d9c17feef90f7a9780e9
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2023-03-11 15:15:08 +0100

    Added login_required to creating articles

--- articles/urls.py
+++ articles/urls.py
@@ -5,6 +5,7 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.article_list, name="list"),
+    path('create/', views.article_create, name="create"),
     path('<slug:slug>/', views.article_detail, name="detail")
     
 ]
